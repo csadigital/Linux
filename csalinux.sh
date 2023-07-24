57f914fbda74b25a8cd66d7574c5dd1872e0cda7
CSA Digital <130220642+csadigital@users.noreply.github.com>
2023-07-24 03:16:38 +0300

    Update csalinux.sh

--- csalinux.sh
+++ csalinux.sh
@@ -1,85 +1,41 @@
 #!/bin/bash
 
-# Renk tanımlamaları
+# Renkli yazılar için renk tanımlamaları
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
-RED='\033[0;31m'
-NC='\033[0m' # Renk sıfırlama
-
-# Fonksiyon: Gerekli araçları yükle
-function install_required_tools() {
-    echo -e "${GREEN}Gerekli araçlar yükleniyor...${NC}"
-    sudo yum install git curl nano -y
-    echo -e "${GREEN}Gerekli araçlar başarıyla yüklendi.${NC}"
-}
-
-# Fonksiyon: Bash Yükleme
-
-function install_bashtop() {
-    echo -e "${GREEN}Bashtop yükleniyor...${NC}"
-    git clone https://github.com/aristocratos/bashtop.git
-    cd bashtop
-    sudo make install
-    echo -e "${GREEN}Bashtop başarıyla yüklendi.${NC}"
-}
-
-# Gerekli bağımlılıkları yükle
-echo -e "${GREEN}Gerekli bağımlılıklar yükleniyor...${NC}"
-# Eğer sisteminizde Python3 ve pip3 yoksa, aşağıdaki komutları kullanarak yükleyebilirsiniz.
-sudo apt-get update
-sudo apt-get install -y python3 python3-pip
-
-# Python için yapılan yüklemelerden sonra pip kullanarak bashtop'u yükleyebilirsiniz.
- 
-
- 
-
-# Fonksiyon: Bashtop'u Sistem Monitörü olarak başlat
-function start_system_monitor() {
-    echo -e "${GREEN}Sistem Monitörü başlatılıyor...${NC}"
-    bashtop
-}
+NC='\033[0m' # No Color
 
 # Fonksiyon: CSF Algılama Modu Aç
 function enable_csf_detection() {
     echo -e "${GREEN}CSF Algılama Modu açılıyor...${NC}"
     chkconfig --levels 235 csf on
     chkconfig --levels 235 lfd on
-    echo -e "${GREEN}CSF Algılama Modu başarıyla açıldı.${NC}"
 }
 
 # Fonksiyon: CSF Algılama Modu Kapat
 function disable_csf_detection() {
     echo -e "${GREEN}CSF Algılama Modu kapatılıyor...${NC}"
-     chkconfig --levels 235 csf off
-     chkconfig --levels 235 lfd off
-    echo -e "${GREEN}CSF Algılama Modu başarıyla kapatıldı.${NC}"
+    chkconfig --levels 235 csf off
+    chkconfig --levels 235 lfd off
 }
 
 # Fonksiyon: CSF Kurulum ve Ayarlar
 function install_csf() {
     echo -e "${GREEN}CSF kurulumu ve ayarları yapılıyor...${NC}"
-    sudo yum install wget perl-libwww-perl perl-LWP-Protocol-https -y
-    cd /usr/src
-    sudo rm -fv csf.tgz
-    sudo wget https://download.configserver.com/csf.tgz
-    sudo tar -xzf csf.tgz
-    cd csf
-    sudo sh install.sh
-    echo -e "${GREEN}CSF kurulumu ve ayarları tamamlandı.${NC}"
+    curl -Ls https://raw.githubusercontent.com/csadigital/Linux/main/csfinstall.sh | bash
 }
 
 # Fonksiyon: Litespeed Ayarlar
 function configure_litespeed() {
     echo -e "${GREEN}Litespeed ayarları yapılıyor...${NC}"
     wget -O /usr/local/lsws/conf/httpd_config.xml https://raw.githubusercontent.com/csadigital/Linux/main/httpd_config.xml
-    sudo chown lsadm:lsadm /usr/local/lsws/conf/httpd_config.xml
-    sudo chmod 644 /usr/local/lsws/conf/httpd_config.xml
+    chown lsadm:lsadm /usr/local/lsws/conf/httpd_config.xml
+    chmod 644 /usr/local/lsws/conf/httpd_config.xml
 }
 
 # Fonksiyon: SSH Ayarlar
 function configure_ssh() {
-    echo -e "${GREEN}SSH Portu 2220 Olarak Değiştiriliyor ...${NC}"
+    echo -e "${GREEN}SSH ayarları yapılıyor...${NC}"
     sudo sed -i 's/#Port 22/Port 2220/' /etc/ssh/sshd_config
     sudo systemctl restart sshd
 }
@@ -108,135 +64,41 @@ function show_free_ram() {
     free -h
 }
 
-# Fonksiyon: Hostname Değiştirme
-function change_hostname() {
-    echo -e "${GREEN}Yeni hostname girin: ${NC}"
-    read new_hostname
-    echo "$new_hostname" | sudo tee /etc/hostname
-    sudo hostnamectl set-hostname "$new_hostname"
-    echo -e "${GREEN}Hostname başarıyla değiştirildi.${NC}"
-}
-
-# Fonksiyon: SSH Portu Değiştirme
-function change_ssh_port() {
-    echo -e "${GREEN}Yeni SSH portu numarası girin: ${NC}"
-    read new_ssh_port
-    sudo sed -i "s/^Port .*/Port $new_ssh_port/" /etc/ssh/sshd_config
-    sudo systemctl restart sshd
-    echo -e "${GREEN}SSH portu başarıyla değiştirildi. Yeni port: $new_ssh_port${NC}"
-}
-
-# Fonksiyon: Litespeed Restart
-function restart_litespeed() {
-    echo -e "${GREEN}Litespeed yeniden başlatılıyor...${NC}"
-    sudo systemctl restart lsws
-    echo -e "${GREEN}Litespeed başarıyla yeniden başlatıldı.${NC}"
-}
-
-# Fonksiyon: Apache Restart
-function restart_apache() {
-    echo -e "${GREEN}Apache yeniden başlatılıyor...${NC}"
-    sudo systemctl restart apache2
-    echo -e "${GREEN}Apache başarıyla yeniden başlatıldı.${NC}"
-}
-
-# Fonksiyon: MySQL Optimize
-function optimize_mysql() {
-    echo -e "${GREEN}MySQL optimize işlemi gerçekleştiriliyor...${NC}"
-    sudo mysqlcheck --auto-repair --optimize --all-databases
-    echo -e "${GREEN}MySQL optimize işlemi tamamlandı.${NC}"
-}
-
-# Fonksiyon: cPanel Kurulumu CentOS ve AlmaLinux İçin
-function install_cpanel() {
-    echo -e "${GREEN}cPanel kurulumu yapılıyor...${NC}"
-    cd /home
-    curl -o latest -L https://securedownloads.cpanel.net/latest
-    sh latest
-}
-
-# Fonksiyon: DirectAdmin Kurulumu CentOS ve AlmaLinux İçin
-function install_directadmin() {
-    echo -e "${GREEN}DirectAdmin kurulumu yapılıyor...${NC}"
-    wget -O setup.sh https://www.directadmin.com/setup.sh
-    chmod 755 setup.sh
-    ./setup.sh
-}
-
-# Fonksiyon: CyberPanel Kurulumu CentOS ve AlmaLinux İçin
-function install_cyberpanel() {
-    echo -e "${GREEN}CyberPanel kurulumu yapılıyor...${NC}"
-    sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh)
-}
-
-# Fonksiyon: CentOS Web Panel (CWP) Kurulumu CentOS İçin
-function install_cwp() {
-    echo -e "${GREEN}CentOS Web Panel (CWP) kurulumu yapılıyor...${NC}"
-    wget http://centos-webpanel.com/cwp-el7-latest
-    sh cwp-el7-latest
-}
-
-# Fonksiyon: Plesk Panel Kurulumu CentOS ve AlmaLinux İçin
-function install_pleskpanel() {
-    echo -e "${GREEN}Plesk Panel kurulumu yapılıyor...${NC}"
-    sh <(curl https://installer.plesk.com/plesk-installer || wget -O - https://installer.plesk.com/plesk-installer)
-}
-
 # Ana menü
 function main_menu() {
-    while true
-    do
-        clear
-        echo -e "${CYAN}========== CSA Linux Bot - Linux Ayar Scripti ==========${NC}"
-        echo -e "${GREEN}1. Gerekli Araçları Yükle (git, curl, nano)"
-        echo "2. Bash Yükle"
-        echo "3. Sistem Monitörü Olarak Başlat (Bashtop)"
-        echo "4. CSF Algılama Modu Aç"
-        echo "5. CSF Algılama Modu Kapat"
-        echo "6. CSF Kurulum ve Ayarlar"
-        echo "7. Litespeed Ayarlar"
-        echo "8. SSH Ayarlar"
-        echo "9. Swap Performans Kernel"
-        echo "10. CSF Katı DDoS Ayarları"
-        echo "11. Gerçek disk kullanımını görme"
-        echo "12. Boş RAM durumunu görme"
-        echo "13. Hostname Değiştirme"
-        echo "14. SSH Portu Değiştirme"
-        echo "15. Litespeed Restart"
-        echo "16. Apache Restart"
-        echo "17. MySQL Optimize"
-        echo -e "18. Kurulumlar Menüsü"
-        echo -e "0. Çıkış${NC}"
-        echo -n "Seçiminizi girin: "
+    clear
+    echo -e "${CYAN}========== CSA Linux Bot - Linux Ayar Scripti ==========${NC}"
+    echo "1. CSF Algılama Modu Aç"
+    echo "2. CSF Algılama Modu Kapat"
+    echo "3. CSF Kurulum ve Ayarlar"
+    echo "4. Litespeed Ayarlar"
+    echo "5. SSH Ayarlar"
+    echo "6. Swap Performans Kernel"
+    echo "7. CSF Katı DDoS Ayarları"
+    echo "8. Gerçek disk kullanımını görme"
+    echo "9. Boş RAM durumunu görme"
+    echo "0. Çıkış"
+    echo -n "Seçiminizi girin: "
 
-        read choice
+    read choice
 
-        case $choice in
-            1) install_required_tools ;;
-            2) install_bash ;;
-            3) start_system_monitor ;;
-            4) enable_csf_detection ;;
-            5) disable_csf_detection ;;
-            6) install_csf ;;
-            7) configure_litespeed ;;
-            8) configure_ssh ;;
-            9) configure_swap_performance ;;
-            10) configure_csf_ddos ;;
-            11) show_disk_usage ;;
-            12) show_free_ram ;;
-            13) change_hostname ;;
-            14) change_ssh_port ;;
-            15) restart_litespeed ;;
-            16) restart_apache ;;
-            17) optimize_mysql ;;
-            18) installation_menu ;;
-            0) echo -e "${RED}Çıkış yapılıyor.${NC}" ; exit ;;
-            *) echo -e "${RED}Geçersiz seçim. Tekrar deneyin.${NC}" ; sleep 2 ;;
-        esac
+    case $choice in
+        1) enable_csf_detection ;;
+        2) disable_csf_detection ;;
+        3) install_csf ;;
+        4) configure_litespeed ;;
+        5) configure_ssh ;;
+        6) configure_swap_performance ;;
+        7) configure_csf_ddos ;;
+        8) show_disk_usage ;;
+        9) show_free_ram ;;
+        0) exit ;;
+        *) echo -e "${GREEN}Geçersiz seçim. Tekrar deneyin.${NC}" ; sleep 2 ; main_menu ;;
+    esac
 
-        echo -e "${CYAN}İşlem tamamlandı! Devam etmek için Enter tuşuna basın.${NC}"
-        read
-    done
+    echo -e "${CYAN}İşlem tamamlandı! Ana menüye dönülüyor...${NC}"
+    sleep 2
+    main_menu
 }
 
 main_menu
