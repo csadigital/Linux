d90924d2b59cbb355ce9b01d80a32b6dff4fdb82
CSA Digital <130220642+csadigital@users.noreply.github.com>
2023-07-28 03:08:07 +0300

    Update botlinux.sh

--- botlinux.sh
+++ botlinux.sh
@@ -23,35 +23,6 @@ set key autotitle columnheader
 plot "-" using 1:2 with lines
 '
 
-# Telegram bot script dosyası
-telegram_bot_script='
-#!/bin/bash
-
-# Telegram botunuzun token'ı ve chat ID'sini burada tanımlayın
-TELEGRAM_TOKEN="YOUR_TELEGRAM_BOT_TOKEN"
-TELEGRAM_CHAT_ID="YOUR_TELEGRAM_CHAT_ID"
-
-# Telegram'a mesaj gönderen fonksiyon
-send_telegram_message() {
-  local message="$1"
-  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=$message" >/dev/null
-}
-
-# Sunucu yükünü kontrol eden fonksiyon
-check_load_average() {
-  local load_average=$(uptime | awk -F'load average:' '{print $2}' | xargs)
-  local threshold=3.0
-
-  if (( $(echo "$load_average >= $threshold" | bc -l) )); then
-    local message="Sunucu yükü yüksek! Load Average: $load_average"
-    send_telegram_message "$message"
-  fi
-}
-
-# Ana programı çalıştır
-check_load_average
-'
-
 # İstatistikleri belirli aralıklarla alacak fonksiyon
 get_current_stats() {
   local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
@@ -69,6 +40,12 @@ generate_gnuplot_script() {
   printf "$gnuplot_script_template" > "$BASE_DIR/gnuplot_script.plt"
 }
 
+# Telegram bot'a mesaj gönderen fonksiyon
+send_telegram_message() {
+  local message="$1"
+  curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID&text=$message" >/dev/null
+}
+
 # Sunucu yükünü kontrol eden fonksiyon
 check_load_average() {
   local load_average=$(uptime | awk -F'load average:' '{print $2}' | xargs)
@@ -76,7 +53,7 @@ check_load_average() {
 
   if (( $(echo "$load_average >= $threshold" | bc -l) )); then
     local message="Sunucu yükü yüksek! Load Average: $load_average"
-    bash -c "send_telegram_message '$message'"
+    send_telegram_message "$message"
   fi
 }
 
@@ -104,7 +81,7 @@ start_server_monitor() {
     cat "$log_file" | tail -n 10 | gnuplot --persist "$BASE_DIR/gnuplot_script.plt"
 
     # Yüksek yükü kontrol et ve Telegram bildirim gönder
-    bash -c "$telegram_bot_script"
+    check_load_average
 
     # 5 saniyede bir döngüyü tekrarla
     sleep 5
